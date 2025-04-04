507148278289687ec73cdb527e22155c36e60192
Vinicius Catafesta Francisco <vinicfrancisco@gmail.com>
2025-04-04 11:18:19 -0300

    feat: add button component

---
+++ src/app/components/ui/button.tsx
@@ -0,0 +1,28 @@
+import { cn } from "@/app/lib/utils";
+import { ButtonHTMLAttributes, PropsWithChildren } from "react";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant: "primary" | "secondary" | "ghost";
+}
+
+export default function Button({
+  children,
+  variant = "primary",
+  className,
+  ...props
+}: PropsWithChildren<ButtonProps>) {
+  return (
+    <button
+      className={cn(
+        "p-3 text-white rounded-xl font-bold whitespace-nowrap hover:opacity-95 disabled:opacity-70",
+        variant === "primary" && "bg-accent-purple",
+        variant === "secondary" && "bg-background-tertiary",
+        variant === "ghost" && "border-border-primary",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
