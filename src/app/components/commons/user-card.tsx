16fa5825930d772c69dc27b81e2e264d71b5b1d1
Vinicius Catafesta Francisco <vinicfrancisco@gmail.com>
2025-04-07 16:27:34 -0300

    feat: add total visits

--- src/app/components/commons/user-card.tsx
+++ src/app/components/commons/user-card.tsx
@@ -1,5 +1,6 @@
 import { Github, Instagram, Linkedin, Plus, Twitter } from "lucide-react";
 import Button from "../ui/button";
+import Image from "next/image";
 
 export default function UserCard() {
   const icons = [Github, Instagram, Linkedin, Twitter, Plus];
@@ -7,24 +8,31 @@ export default function UserCard() {
   return (
     <div className="w-[348px] flex flex-col gap-5 items-center p-5 border border-white border-opacity-10 bg-[#121212] rounded-3xl text-white">
       <div className="size-48">
-        <img
+        <Image
           src="/me.jpg"
           alt="Vini Dev"
+          width={348}
+          height={348}
           className="rounded-full object-cover w-full h-full"
         />
       </div>
+
       <div className="flex flex-col gap-2 w-full">
         <div className="flex items-center gap-2">
           <h3 className="text-3xl font-bold min-w-0 overflow-hidden">
             Vini Dev
           </h3>
         </div>
-        <p className="opacity-40">"Eu faço produtos para a Internet"</p>
+
+        <p className="opacity-40">
+          &quot;Eu faço produtos para a Internet&quot;
+        </p>
       </div>
+
       <div className="flex flex-col gap-2 w-full">
         <span className="uppercase text-xs font-medium">Links</span>
 
-        <div className="flex gap-3 justify-around">
+        <div className="flex gap-3">
           {icons.map((Icon, index) => (
             <button
               key={index}
@@ -39,6 +47,7 @@ export default function UserCard() {
       <div className="flex flex-col gap-3 w-full h-[172px]">
         <div className="w-full flex flex-col items-center gap-3">
           <Button className="w-full">Template SaaS - Compre Agora</Button>
+
           <button className="p-3 rounded-xl bg-[#1E1E1E] hover:bg-[#2E2E2E]">
             <Plus />
           </button>
--- src/app/components/landing-page/hero.tsx
+++ src/app/components/landing-page/hero.tsx
@@ -1,19 +1,20 @@
+import TotalVisits from "../commons/total-visits";
 import UserCard from "../commons/user-card";
 import Button from "../ui/button";
 import TextInput from "../ui/text-input";
 
 export default function Hero() {
   return (
-    <div className="flex">
-      <div className="flex w-full flex-col gap-2 mt-[35vh]">
+    <div className="flex h-screen">
+      <div className="w-full flex flex-col gap-2 mt-[35vh]">
         <h1 className="text-5xl font-bold text-white leading-[64px]">
-          Seus projetos e redes sociais em um único link.
+          Seus projetos e redes sociais em um único link
         </h1>
 
         <h2 className="text-xl leading-6">
           Crie sua própria página de projetos e compartilhe eles com o mundo.
           <br />
-          Acompanhe o engajamento com Analytics de cliques.
+          Acompanhe o engajamento com Analytics de cliques
         </h2>
 
         <div className="flex items-center gap-2 w-full mt-[10vh]">
@@ -26,12 +27,15 @@ export default function Hero() {
       <div className="w-full flex items-center justify-center bg-[radial-gradient(circle_at_50%_50%,#4B2DBB,transparent_55%)]">
         <div className="relative">
           <UserCard />
+
           <div className="absolute -bottom-[7%] -right-[45%]">
-            {/* <TotalVisits /> */}
+            <TotalVisits />
           </div>
+
           <div className="absolute top-[20%] -left-[45%] -z-10">
             {/* <ProjectCard /> */}
           </div>
+
           <div className="absolute -top-[5%] -left-[55%] -z-10">
             {/* <ProjectCard /> */}
           </div>
