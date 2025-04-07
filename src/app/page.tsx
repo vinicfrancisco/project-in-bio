ebb8c41b5d26456a07ce83e3191c5b9cb16cdbd4
Vinicius Catafesta Francisco <vinicfrancisco@gmail.com>
2025-04-07 17:30:59 -0300

    feat: add FAQ component to LP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import FAQ from "./components/landing-page/faq";
 import Header from "./components/landing-page/header";
 import Hero from "./components/landing-page/hero";
 import Pricing from "./components/landing-page/pricing";
@@ -10,7 +11,7 @@ export default function Home() {
       <Header />
       <VideoExplanation />
       <Pricing />
-      {/*  <FAQ /> */}
+      <FAQ />
     </div>
   );
 }
