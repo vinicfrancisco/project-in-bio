ad2b3e13fbc8e5f467fe6b96f9221ffe2f38e140
Vinicius Catafesta Francisco <vinicfrancisco@gmail.com>
2025-04-08 09:10:38 -0300

    feat: add upgrade page

--- src/app/(pages)/[profileId]/page.tsx
+++ src/app/(pages)/[profileId]/page.tsx
@@ -2,20 +2,25 @@ import ProjectCard from "@/app/components/commons/project-card";
 import TotalVisits from "@/app/components/commons/total-visits";
 import UserCard from "@/app/components/commons/user-card";
 import { Plus } from "lucide-react";
+import Link from "next/link";
 
-export default async function ProfilePage({}: {
+export default async function ProfilePage({
+  params,
+}: {
   params: { profileId: string };
 }) {
-  //   const { profileId } = await params;
+  const { profileId } = await params;
 
   return (
     <div className="relative h-screen flex p-20 overflow-hidden">
       <div className="fixed top-0 left-0 w-full flex justify-center items-center gap-1 py-2 bg-background-tertiary">
         <span>Você está usando a versão trial.</span>
 
-        <button className="text-accent-green font-bold">
-          Faça o upgrade agora
-        </button>
+        <Link href={`/${profileId}/upgrade`}>
+          <button className="text-accent-green font-bold">
+            Faça o upgrade agora
+          </button>
+        </Link>
       </div>
 
       <div className="w-1/2 flex justify-center h-min">
