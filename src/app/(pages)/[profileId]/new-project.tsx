211818a8e6f560216454b71ace78347e279983ee
Vinicius Catafesta Francisco <vinicfrancisco@gmail.com>
2025-04-09 10:37:47 -0300

    feat: add create project logic

--- src/app/(pages)/[profileId]/new-project.tsx
+++ src/app/(pages)/[profileId]/new-project.tsx
@@ -1,20 +1,77 @@
 "use client";
 
+import { createProject } from "@/app/actions/create-project";
 import Button from "@/app/components/ui/button";
 import Modal from "@/app/components/ui/modal";
 import TextArea from "@/app/components/ui/text-area";
 import TextInput from "@/app/components/ui/text-input";
+import { compressFiles } from "@/app/lib/utils";
 import { ArrowUpFromLine, Plus } from "lucide-react";
-import { useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { ChangeEvent, startTransition, useState } from "react";
 
 interface NewProjectProps {
   profileId: string;
 }
 
 export default function NewProject({ profileId }: NewProjectProps) {
+  const router = useRouter();
+
   const [isOpen, setIsOpen] = useState(false);
+  const [projectName, setProjectName] = useState("");
+  const [projectDescription, setProjectDescription] = useState("");
+  const [projectImage, setProjectImage] = useState<string | null>(null);
+  const [projectUrl, setProjectUrl] = useState("");
+  const [isCreatingProject, setIsCreatingProject] = useState(false);
 
   const openModal = () => setIsOpen(true);
+
+  const triggerImageInput = (inputId: string) =>
+    document.getElementById(inputId)?.click();
+
+  const handleImageInput = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0] ?? null;
+
+    if (file) {
+      return setProjectImage(URL.createObjectURL(file));
+    }
+
+    return setProjectImage(null);
+  };
+
+  const handleSaveProject = async () => {
+    setIsCreatingProject(true);
+
+    const imageInput = document.getElementById(
+      "imageInput"
+    ) as HTMLInputElement;
+
+    if (!imageInput.files?.length) return;
+
+    const compressedFile = await compressFiles(Array.from(imageInput.files));
+
+    const formData = new FormData();
+
+    formData.append("file", compressedFile?.[0]);
+    formData.append("profileId", profileId);
+    formData.append("projectName", projectName);
+    formData.append("projectDescription", projectDescription);
+    formData.append("projectUrl", projectUrl);
+
+    await createProject(formData);
+
+    startTransition(() => {
+      setIsOpen(false);
+      setIsCreatingProject(false);
+      setProjectName("");
+      setProjectDescription("");
+      setProjectUrl("");
+      setProjectImage(null);
+
+      router.refresh();
+    });
+  };
 
   return (
     <>
@@ -34,10 +91,28 @@ export default function NewProject({ profileId }: NewProjectProps) {
           <div className="flex gap-10">
             <div className="flex flex-col items-center gap-3 text-xs">
               <div className="size-[100px] rounded-xl bg-background-tertiary overflow-hidden">
-                <button className="size-full">100x100</button>
+                {projectImage ? (
+                  <Image
+                    height={100}
+                    width={100}
+                    src={projectImage}
+                    alt="Project Image"
+                    className="object-cover object-center"
+                  />
+                ) : (
+                  <button
+                    className="size-full"
+                    onClick={() => triggerImageInput("imageInput")}
+                  >
+                    100x100
+                  </button>
+                )}
               </div>
 
-              <button className="text-white flex items-center gap-2">
+              <button
+                className="text-white flex items-center gap-2"
+                onClick={() => triggerImageInput("imageInput")}
+              >
                 <ArrowUpFromLine className="size-4" />
 
                 <span>Adicionar imagem</span>
@@ -48,6 +123,7 @@ export default function NewProject({ profileId }: NewProjectProps) {
                 id="imageInput"
                 accept="image/*"
                 className="hidden"
+                onChange={handleImageInput}
               />
             </div>
 
@@ -59,6 +135,8 @@ export default function NewProject({ profileId }: NewProjectProps) {
 
                 <TextInput
                   id="project-name"
+                  value={projectName}
+                  onChange={(e) => setProjectName(e.target.value)}
                   placeholder="Digite o nome do projeto"
                 />
               </div>
@@ -73,6 +151,8 @@ export default function NewProject({ profileId }: NewProjectProps) {
 
                 <TextArea
                   id="project-description"
+                  value={projectDescription}
+                  onChange={(e) => setProjectDescription(e.target.value)}
                   placeholder="Dê uma breve descrição do seu projeto"
                   className="h-36"
                 />
@@ -86,6 +166,8 @@ export default function NewProject({ profileId }: NewProjectProps) {
                 <TextInput
                   id="project-url"
                   type="url"
+                  value={projectUrl}
+                  onChange={(e) => setProjectUrl(e.target.value)}
                   placeholder="Digite a URL do projeto"
                 />
               </div>
@@ -93,9 +175,16 @@ export default function NewProject({ profileId }: NewProjectProps) {
           </div>
 
           <div className="flex gap-4 justify-end">
-            <button className="font-bold text-white">Voltar</button>
+            <button
+              className="font-bold text-white"
+              onClick={() => setIsOpen(false)}
+            >
+              Voltar
+            </button>
 
-            <Button>Salvar</Button>
+            <Button onClick={handleSaveProject} disabled={isCreatingProject}>
+              Salvar
+            </Button>
           </div>
         </div>
       </Modal>
--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -1,5 +1,6 @@
 import { ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import imageCompression, { Options } from "browser-image-compression";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -12,4 +13,31 @@ export function sanitizeLink(link?: string) {
     .replace(/\s/g, "")
     .replace(/[!@#$%^&*()_+\-=\[\]{};':"\\|,ˆ.<>\/?]+/, "")
     .toLocaleLowerCase();
+}
+
+export const compressImage = (file: File): Promise<File> => {
+  return new Promise((resolve) => {
+    const options: Options = {
+      maxSizeMB: 0.2, // 200KB
+      maxWidthOrHeight: 900,
+      useWebWorker: true,
+      fileType: "image/png",
+    };
+
+    imageCompression(file, options).then((compressedFile) =>
+      resolve(compressedFile)
+    );
+  });
+};
+
+export async function compressFiles(files: File[]) {
+  const compressPromises = files.map(async (file) => {
+    try {
+      return await compressImage(file);
+    } catch {
+      return null;
+    }
+  });
+
+  return (await Promise.all(compressPromises)).filter((file) => file !== null);
 }
